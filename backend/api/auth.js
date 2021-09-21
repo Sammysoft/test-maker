db1dfc277f7258ca01f39cfb1fa53407a7abb877
Samuel Dare-Owonibi <dareowonibisamuel@gmail.com>
2021-09-21 21:11:52 +0000

    added an authentication middleware using jwt web token

--- backend/api/auth.js
+++ backend/api/auth.js
@@ -1,11 +1,12 @@
 
 const bcrypt = require('bcryptjs');
 const User = require('../models/simpleUser');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 module.exports = {
      _loginUser: async (req,res,next)=> {
           const { username,password } = req.body;
-          console.log(username)
           const user = await User.findOne({username: username})
           if(!user){
                res.status(400).json('Wrong Credentials')
@@ -14,7 +15,14 @@ module.exports = {
                try {
                     bcrypt.compare( password, user.password, (err, isMatch)=>{
                         if(isMatch){
-                            res.status(200).json( user )
+                             const accesstoken = jwt.sign(
+                                  {
+                                       id: user._id,
+                                       isAdmin: user.isAdmin
+                                  }, process.env.TOKEN_KEY, { expiresIn: "1d" }
+                             )
+                             const { password, ...others } = user._doc
+                            res.status(200).json( {...others, accesstoken} )
                         }else{
                             res.status(400).json('Wrong password')
                         }
